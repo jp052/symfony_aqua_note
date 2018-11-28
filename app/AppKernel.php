9d4e3c0ea9383a49a94ddd69f876146aa97d966c
jp052 <jan.p2001@gmail.com>
2018-11-28 11:29:48 +1030

    Add funFact variable with markdown support

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            //3rd party
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+
+            //custom
             new AppBundle\AppBundle(),
         ];
 
--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,6 +14,13 @@ class GenusController extends Controller
     public function showAction($genusName)
 
     {
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        //quick access to container via just using get
+        $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+
+
         $notes = [
             'Octopus asked me a riddle, outsmarted me',
             'I counted 8 legs... as they wrapped around me',
@@ -22,7 +29,8 @@ class GenusController extends Controller
 
         return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
-            'notes' => $notes
+            'notes' => $notes,
+            'funFact' => $funFact
         ));
     }
 }
